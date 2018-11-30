3226f469e9e39e66b67c12f3a923a6b5a2c33d48
Bruno Krebs <bruno.krebs@auth0.com>
2018-11-30 15:46:10 -0200

    updating final API solution;

--- lab-02/end/api/api-server.js
+++ lab-02/end/api/api-server.js
@@ -1,29 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const jwtAuthz = require('express-jwt-authz');
+const { auth, strategies, requiredScopes } = require('express-oauth2-bearer');
 
 const appUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
 
 const app = express();
 
-app.use(jwt({
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
+app.use(auth(strategies.openid()));
 
-  // Validate the audience and the issuer.
-  audience: process.env.API_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
-}));
-
-app.get('/', jwtAuthz(['reports:read']), (req, res) => {
+app.get('/', requiredScopes('reports:read'), (req, res) => {
   res.send([
     {
       date: new Date(),
