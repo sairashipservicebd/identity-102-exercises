988b27fa5b820d90d41c67b3622ba2b7c971057c
Bruno Krebs <bruno.krebs@auth0.com>
2018-12-10 19:52:59 -0200

    updating error handling;

--- lab-02/begin/webapp/server.js
+++ lab-02/begin/webapp/server.js
@@ -35,30 +35,30 @@ app.get('/user', requiresAuth(), (req, res) => {
   res.render('user', { user: req.openid && req.openid.user });
 });
 
-app.get('/expenses', requiresAuth(), errorHandler(async (req, res) => {
-  const expenses = await request(process.env.API_URL, {
-    json: true
-  });
+app.get('/expenses', requiresAuth(), async (req, res, next) => {
+  try {
+    const expenses = await request(process.env.API_URL, {
+      json: true
+    });
 
-  res.render('expenses', {
-    user: req.openid && req.openid.user,
-    expenses,
-  });
-}));
+    res.render('expenses', {
+      user: req.openid && req.openid.user,
+      expenses,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 app.get('/logout', (req, res) => {
   req.session = null;
   res.redirect('/');
 });
 
-function errorHandler(fn) {
-  return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => {
-      console.error(err.stack);
-      res.status(500).send(err);
-    });
-  };
-}
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(err);
+});
 
 http.createServer(app).listen(process.env.PORT, () => {
   console.log(`listening on ${appUrl}`);
