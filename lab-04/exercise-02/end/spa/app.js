29cd74e0510002348982d79d71dadfba118ebac1
Bruno Krebs <bruno.krebs@auth0.com>
2019-01-24 10:46:08 -0200

    adding more comments to the final solution;

--- lab-04/exercise-02/end/spa/app.js
+++ lab-04/exercise-02/end/spa/app.js
@@ -1,4 +1,5 @@
 window.onload = async function() {
+  // UI/DOM elements
   const content = document.getElementById('content');
   const loadingIndicator = document.getElementById('loading-indicator');
   const loginButton = document.getElementById('log-in');
@@ -11,28 +12,31 @@ window.onload = async function() {
   const userFullname = document.getElementById('user-fullname');
   const userEmail = document.getElementById('user-email');
 
+  // configuring the Auth0 library
   const auth0Client = new Auth0Login({
     domain: 'labs102-bk.auth0.com',
     client_id: 'AT6YZLCKQu1llsqoE55TfEBHsITyvZIf'
   });
 
+  // there is no need to call `init` when the request is a callback from Auth0
   if (window.location.hash === '#callback') {
     await auth0Client.handleRedirectCallback();
-    await showProfile();
+    await renderContent();
     window.history.replaceState({}, document.title, '/');
   } else {
+    // if it is not a callback, then we check if there is a session at Auth0
     await auth0Client.init();
-    await showProfile();
+    await renderContent();
   }
 
-  // configuring the login button
+  // making the login button redirect users to the Auth0 Login Page
   loginButton.onclick = async () => {
     await auth0Client.loginWithRedirect({
       redirect_uri: 'http://localhost:5000/#callback'
     });
   };
 
-  // configuring the logout button
+  // making the logout button terminate the session at Auth0
   logoutButton.onclick = () => {
     auth0Client.logout({
       client_id: 'AT6YZLCKQu1llsqoE55TfEBHsITyvZIf',
@@ -40,7 +44,7 @@ window.onload = async function() {
     });
   };
 
-  // configuring the fetch access token button
+  // making the "fetch expenses" button get an access token and load the expenses from the API
   fetchTokenButton.onclick = async () => {
     const expensesAPIOptions = {
       audience: 'https://expenses-api',
@@ -71,8 +75,8 @@ window.onload = async function() {
     expensesContainer.style.display = 'block';
   };
 
-  // function to render the user profile on the page
-  async function showProfile() {
+  // if the user is logged in, shows their profile; otherwise, show the login button
+  async function renderContent() {
     const isAuthenticated = await auth0Client.isAuthenticated();
     if (!isAuthenticated) {
       content.style.display = 'block';
