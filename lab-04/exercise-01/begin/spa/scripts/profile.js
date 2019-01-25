2bb2c487a6e159f4f3f39f72639880158a88b829
Bruno Krebs <bruno.krebs@auth0.com>
2019-01-25 13:03:34 -0200

    removing code that must be added by labs participants;

--- lab-04/exercise-01/begin/spa/scripts/profile.js
+++ lab-04/exercise-01/begin/spa/scripts/profile.js
@@ -4,9 +4,4 @@
   const profilePicture = document.getElementById('profile-picture');
   const userFullname = document.getElementById('user-fullname');
   const userEmail = document.getElementById('user-email');
-
-  const user = await auth0Client.getUser();
-  profilePicture.src = user.picture;
-  userFullname.innerText = user.name;
-  userEmail.innerText = user.email;
 })();
