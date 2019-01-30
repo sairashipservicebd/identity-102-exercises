b41c4943b4ce31fbd4fcf4927462a011ad911407
Bruno Krebs <bruno.krebs@auth0.com>
2019-01-29 22:44:58 -0200

    updating the API code on the final solution of the exercise 2;

--- lab-04/exercise-02/end/api/api-server.js
+++ lab-04/exercise-02/end/api/api-server.js
@@ -8,23 +8,31 @@ const appUrl = process.env.BASE_URL || `http://localhost:${process.env.PORT}`;
 
 const app = express();
 
+const expenses = [
+  {
+    date: new Date(),
+    description: 'Pizza for a Coding Dojo session.',
+    value: 102,
+  },
+  {
+    date: new Date(),
+    description: 'Coffee for a Coding Dojo session.',
+    value: 42,
+  }
+];
+
 app.use(cors());
+
+app.get('/total', (req, res) => {
+  const total = expenses.reduce((accum, expense) => (accum + expense.value), 0);
+  res.send({total, count: expenses.length});
+});
+
 app.use(auth());
 
 app.get('/', requiredScopes('read:reports'), (req, res) => {
   console.log(new Date(req.auth.claims.iat * 1000));
-  res.send([
-    {
-      date: new Date(),
-      description: 'Pizza for a Coding Dojo session.',
-      value: 102,
-    },
-    {
-      date: new Date(),
-      description: 'Coffee for a Coding Dojo session.',
-      value: 42,
-    }
-  ]);
+  res.send(expenses);
 });
 
 http.createServer(app).listen(process.env.PORT, () => {
