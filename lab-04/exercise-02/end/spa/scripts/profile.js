d668a54790e03f672bd29c6bf0e10d0418f846bf
Bruno Krebs <bruno.krebs@auth0.com>
2019-01-30 02:01:56 -0200

    adding the expenses feature to the profile route;

--- lab-04/exercise-02/end/spa/scripts/profile.js
+++ lab-04/exercise-02/end/spa/scripts/profile.js
@@ -9,4 +9,51 @@
   profilePicture.src = user.picture;
   userFullname.innerText = user.name;
   userEmail.innerText = user.email;
+
+  async function loadExpenses(accesstoken) {
+    const response = await fetch('http://localhost:3001/', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Authorization': `Bearer ${accesstoken}`
+      },
+    });
+
+    const expenses = await response.json();
+
+    expenses.forEach(expense => {
+      const newItem = document.createElement('li');
+      const newItemDescription = document.createTextNode(`$ ${expense.value.toFixed(2)} - ${expense.description}`);
+      newItem.appendChild(newItemDescription);
+      expensesList.appendChild(newItem);
+    });
+
+    consentNeededMessage.style.display = 'none';
+    loadExpesesButton.style.display = 'none';
+    expensesContainer.style.display = 'block';
+  }
+
+  const consentNeededMessage = document.getElementById('consent-needed');
+  const expensesContainer = document.getElementById('expenses-container');
+  const expensesList = document.getElementById('expenses-list');
+  const loadExpesesButton = document.getElementById('load-expenses');
+
+  const expensesAPIOptions = {
+    audience: 'https://expenses-api',
+    scope: 'read:reports',
+  };
+
+  let accesstoken;
+  try {
+    accesstoken = await auth0Client.getTokenSilently(expensesAPIOptions);
+    await loadExpenses(accesstoken);
+  } catch (err) {
+    consentNeededMessage.style.display = 'block';
+    loadExpesesButton.style.display = 'inline-block';
+  }
+
+  loadExpesesButton.onclick = async () => {
+    accesstoken = await auth0Client.getTokenWithPopup(expensesAPIOptions);
+    await loadExpenses(accesstoken);
+  };
 })();
