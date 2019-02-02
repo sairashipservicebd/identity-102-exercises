ee8d6acc3b9b22d96d4950a0a2ac39e872d23403
Bruno Krebs <bruno.krebs@auth0.com>
2019-02-02 20:56:32 -0200

    refactoring the expenses.js script;

--- lab-04/exercise-02/end/spa/scripts/expenses.js
+++ lab-04/exercise-02/end/spa/scripts/expenses.js
@@ -4,24 +4,56 @@
   const consentNeeded = document.getElementById('consent-needed');
   const expensesDiv = document.getElementById('expenses-container');
   const expensesList = document.getElementById('expenses-list');
-  const loadExpesesButton = document.getElementById('load-expenses');
+  const loadExpensesButton = document.getElementById('load-expenses');
   const loadingExpenses = document.getElementById('loading-expenses');
 
+  const expensesAPIOptions = {
+    audience: 'https://expenses-api',
+    scope: 'read:reports',
+  };
+
+  try {
+    const accessToken = await auth0Client
+      .getTokenSilently(expensesAPIOptions);
+    await loadExpenses(accessToken);
+  } catch (err) {
+    if (err.error !== 'consent_required') {
+      alert('An error occurred while fetching the access token. Check the browser logs.');
+      return console.log(err);
+    }
+    loadExpensesButton.onclick = async () => {
+      accesstoken = await
+        auth0Client.getTokenWithPopup(expensesAPIOptions);
+      await loadExpenses(accesstoken);
+    };
+
+    consentNeeded.style.display = 'block';
+    loadExpensesButton.style.display = 'inline-block';
+    loadingExpenses.style.display = 'none';
+  }
+
   async function loadExpenses(accesstoken) {
-    const response = await fetch('http://localhost:3001/', {
-      method: 'GET',
-      mode: 'cors',
-      headers: {
-        'Authorization': `Bearer ${accesstoken}`
-      },
-    });
+    try {
+      const response = await fetch('http://localhost:3001/', {
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${accesstoken}`,
+        }
+      });
+      if (!response.ok) throw 'Request status: ' + response.status;
+      const expenses = await response.json();
+      displayExpenses(expenses);
+    } catch (err) {
+      console.log(err);
+      alert('An error occurred while fetching the expenses. Check the browser logs.');
+    }
+  }
 
-    const expenses = await response.json();
-
+  function displayExpenses(expenses) {
     expenses.forEach(expense => {
       const newItem = document.createElement('li');
       const description = `$ ${expense.value.toFixed(2)} ` +
-                          `- ${expense.description}`;
+        `- ${expense.description}`;
       const newItemDescription = document.createTextNode(description);
       newItem.appendChild(newItemDescription);
       expensesList.appendChild(newItem);
@@ -29,29 +61,7 @@
 
     loadingExpenses.style.display = 'none';
     consentNeeded.style.display = 'none';
-    loadExpesesButton.style.display = 'none';
+    loadExpensesButton.style.display = 'none';
     expensesDiv.style.display = 'block';
   }
-
-  const expensesAPIOptions = {
-    audience: 'https://expenses-api',
-    scope: 'read:reports',
-  };
-
-  let accesstoken;
-  try {
-    accesstoken = await
-        auth0Client.getTokenSilently(expensesAPIOptions);
-    await loadExpenses(accesstoken);
-  } catch (err) {
-    consentNeeded.style.display = 'block';
-    loadExpesesButton.style.display = 'inline-block';
-    loadingExpenses.style.display = 'none';
-  }
-
-  loadExpesesButton.onclick = async () => {
-    accesstoken = await
-        auth0Client.getTokenWithPopup(expensesAPIOptions);
-    await loadExpenses(accesstoken);
-  };
 })();
